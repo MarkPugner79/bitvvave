77e14408e6881283aaac146cc8c36a125361308e
RCR11 <123052024+RCR11@users.noreply.github.com>
2023-01-19 00:40:04 -0500

    Updated to use enviromental variables

--- components/Homepage/MessageOfTheDay.vue
+++ components/Homepage/MessageOfTheDay.vue
@@ -2,9 +2,9 @@
   <v-row class="justify-center">
     <div class="d-flex align-center">
       <div class="title text-center mx-3 d-flex flex-column">
-        <span class="font-weight-light white--text">Open Platform Livestreaming</span>
+        <span class="font-weight-light white--text">{{ MOTDText }}</span>
         <div>
-          <span class="overline font-weight-black rgb-text">{{ bottomText }}</span>
+          <span class="overline font-weight-black rgb-text">{{ MOTDDISCLAIMER }}</span>
         </div>
       </div>
 <!--      <img crossorigin="" height="52" data-emote-tooltip=":trout:" title=":trout:" alt=":trout:" src="https://cdn.bitwave.tv/static/emotes/trout-52.gif?4">-->
@@ -20,9 +20,10 @@
 
     data () {
       return {
-        bottomText: process.env.BITWAVE_ENV === 'development'
+        MOTDText: process.env.MOTD.MOTD,
+        MOTDDISCLAIMER: process.env.BITWAVE_ENV === 'development'
           ? 'development build'
-          : 'viewer discretion is advised',
+          : process.env.MOTD.DISCLAIMER,
       }
     },
   };
