58f904c25b049b2b5c92fb09c7e1056527330997
Admin <admin@admin.com>
2023-01-26 09:46:20 -0500

    Maybe we can have streaming back?

--- pages/stream.vue
+++ pages/stream.vue
@@ -33,15 +33,100 @@
           </v-col>
 
           <v-col>
-            <v-btn
-            class="mr-3"
-            color="primary"
-            small
-            
-            @click="startStream"
-          >
-            Start Stream
-          </v-btn>
+            <div class="d-flex flex-shrink-1"
+              :style="{ width: mobile ? '100%' : '450px', 'height': mobile ? '500px' : '555px' }"
+              >
+            <!--<v-layout justify-center> -->
+            <!--<v-flex
+                
+                xs12
+                sm10
+                md8
+                lg6
+            >-->
+                <v-card class="mb-4 pa-3">
+                
+                    <h2>Stream Settings Configuration</h2>
+
+                <v-btn
+                    class="mr-3"
+                    color="primary"
+                    small
+                    
+                    @click="startStreamOldWebRTCMultiConnection"
+                    v-if="this.webRTC"
+                >
+                    Start Stream
+                </v-btn>
+                
+                <v-btn
+                    class="mr-3"
+                    color="secondary"
+                    small
+                    
+                    @click="stopStream"
+                    v-if="this.webRTC"
+                >
+                    Stop Stream
+                </v-btn>
+
+                <v-spacer />
+
+                <h-spacer />
+
+                <!--
+                <v-switch
+                        v-model="this.webRTC"
+                        label="Use WebRTC stream source"
+                        color="primary"
+                        hide-details
+                        dense
+                        inset
+                    />
+                    -->
+
+                    <v-text-field
+                        class="mb-0"
+                        color="primary"
+                        single-line
+                        dense
+                        validate-on-blur
+                        outlined
+                        clearable
+                        id="stream-title"
+                        :label="`Stream Title`"
+                        v-if="this.webRTC"
+                    ></v-text-field>
+                    <v-text-field
+                        class="mb-0"
+                        color="primary"
+                        single-line
+                        dense
+                        validate-on-blur
+                        outlined
+                        clearable
+                        id="stream-desc"
+                        :label="`Stream Description`"
+                        v-if="this.webRTC"
+                    ></v-text-field>
+                    
+                    <v-text-field
+                        class="mb-0"
+                        color="primary"
+                        single-line
+                        dense
+                        validate-on-blur
+                        outlined
+                        clearable
+                        id="stream-rtmp-alt"
+                        :label="`RTMP Backup`"
+
+                        v-if="this.webRTC"
+                    ></v-text-field>
+                </v-card>
+            <!--</v-flex>-->
+        <!--</v-layout>-->
+    </div>
             <!-- <dashboard-superchats /> -->
           </v-col>
 
@@ -64,16 +149,104 @@
 </template>
 
 <script>
-  import { mapGetters, mapState } from 'vuex';
+  import { mapGetters, mapState,mapMutations ,mapActions} from 'vuex';
   import { Chat as ChatStore } from '@/store/chat';
   import { VStore } from '@/store';
+  //import { Player } from '@/store/player';
 
-  import * as io from '@/plugins/socketio.js' // should import v4 of socket io
+  //import * as utils from '@/plugins/store-utils.js';
+
+  /*
+  import {
+    connect,
+    Room,
+    RoomEvent,
+    RemoteParticipant,
+    RemoteTrackPublication,
+    RemoteTrack,
+    Participant,
+    VideoCaptureOptions,
+    VideoCodec,
+    VideoPresets,
+    VideoQuality,
+    } from 'livekit-client';
+    */
+
+
+    // use the chat server to get our stream key
+   
+    import * as io from '@/plugins/socketio.js'; // should import v4 of socket io
+
+    
+
+   
+    /*
+    let socketlc = io.connect(process.env.CHATSERVER, { transports: ['websocket'] });
+
+    let liveKitToken = null;
+    let usertoken = "";
+
+    socketlc.on('connect', () => {
+        function parseToken (token) {
+          try{
+              var base64Url = token.split('.')[1];
+              var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+              var jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
+                  return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+              }).join(''));
+          }catch(ex){
+              console.log("failed to parse the token")
+              return {user:{name:'failed'}}
+          }
+          return JSON.parse(jsonPayload);
+      };
+        //load_chatToken();
+        if (localStorage.getItem('troll')) {
+            usertoken = localStorage.getItem('troll');
+            console.log("Should have decoded a token:", usertoken);
+            // should then parse
+            let user = parseToken( usertoken );
+            console.log("Decoded user:",user.sub.username);
+            //this.streamerName = user;
+        }
+        if (localStorage.getItem('chatToken')) {
+            usertoken = localStorage.getItem('chatToken');
+            console.log("Should have decoded a token:", usertoken);
+            // should then parse
+            let user = parseToken( usertoken );
+            console.log("Decoded user:",user.sub.username);
+            //this.streamerName = user;
+        }
+
+        socketlc.emit('new user', {jwt:usertoken});
+        //socketlc.emit('get live kit room token', {jwt:usertoken});
+        
+        //document.getElementById('broadcast-id').disabled = true;
+        
+        //socketlc.emit('getlivestreams');
+        //if(was_live){
+        //  console.log("Said that we were streaming and that we can still be connected to.");
+          //socketlc.emit('announcestream');
+        //}
+        
+    });
+
+    socketlc.on('room access token',(data) => {
+        console.log("Should have got back a live kit token:",data);
+        // should then enable the start broadcast livekit button
+        liveKitToken = data.token;
+
+    });
+    */
+
+     
+
+  
 
     //const RTCMultiConnection = require('@/plugins/simpleRTC.js'); // so this seems to work after breaking the export system
     import * as RTCMultiConnection from '@/plugins/simpleRTC.js'
 
-    import jwt_decode from 'jwt-decode';
+    //import jwt_decode from 'jwt-decode';
     let connection = new RTCMultiConnection(); // so this fires the second instance it would seem
 
     window.io = io;
@@ -203,18 +376,7 @@
                     connection.getAllParticipants().forEach(function(p) {
                         if (p + '' != event.userid + '') {
                             connection.replaceTrack(event.stream, p);
-                            /* https://github.com/muaz-khan/RTCMultiConnection/issues/927
-                            var peer = connection.peers[p].peer;
-                            peer.getLocalStreams().forEach(function(localStream) {
-                                peer.removeStream(localStream);
-                            });
-                            event.stream.getTracks().forEach(function(track) {
-                                peer.addTrack(track, event.stream);
-                            });
-                            connection.dontAttachStream = true;
-                            connection.renegotiate(p);
-                            connection.dontAttachStream = false;
-                            */
+                            
                         }
                     });
                 }
@@ -319,8 +481,8 @@
 
         console.log('View count:',event.numberOfBroadcastViewers);
         //document.getElementById('broadcast-viewers-counter').innerHTML = 'Number of broadcast viewers: <b>' + event.numberOfBroadcastViewers + '</b>';
-        let broadcastDesc = 'This description was updated in the view count update';//document.getElementById('broadcast-desc').value;
-        let rtmpstream = 'This should be the rtmp stream fall back';// document.getElementById('rtmp').value;
+        let broadcastDesc = document.getElementById('stream-desc').value;//'This description was updated in the view count update';//document.getElementById('broadcast-desc').value;
+        let rtmpstream = document.getElementById('stream-rtmp-alt').value;//'This should be the rtmp stream fall back';// document.getElementById('rtmp').value;
         socketlc.emit('announcestream',{desc:broadcastDesc,viewers:event.numberOfBroadcastViewers,src:rtmpstream});
     };
 
@@ -374,12 +536,7 @@
     });
 
     function doStreamConnect(streamid){
-        /*var broadcastId = document.getElementById('broadcast-id').value;
-        if (broadcastId.replace(/^\s+|\s+$/g, '').length <= 0) {
-            alert('Please enter broadcast-id');
-            document.getElementById('broadcast-id').focus();
-            return;
-        }*/
+        
         let disable_stream_connect = false;
         disable_stream_connect = false;
         //document.getElementById("livestreams").disabled = true;
@@ -417,7 +574,7 @@
     }
 
     } // end up setup check
-
+     // end of connection setup
   export default {
     name: 'dashboard',
 
@@ -428,12 +585,96 @@
         mounted: false,
         videoPreview: null,
         streamerName:null,
+        webRTC:null,
       };
     },
 
     methods: {
-        startStream(){
 
+        async startStream () {
+            /*
+            console.log("Should setup the LiveKit Stream");
+            // https://github.com/livekit/client-sdk-js
+            console.log(`WebRTC state: ${this.webRTC}`);
+            // creates a new room with options
+            const room = new Room({
+            // automatically manage subscribed video quality
+            adaptiveStream: true,
+
+            // optimize publishing bandwidth and CPU for published tracks
+            dynacast: true,
+
+            // default capture settings
+            videoCaptureDefaults: {
+                resolution: VideoPresets.h720.resolution,
+            },
+            });
+
+            // set up event listeners
+            room
+            .on(RoomEvent.TrackSubscribed, handleTrackSubscribed)
+            .on(RoomEvent.TrackUnsubscribed, handleTrackUnsubscribed)
+            .on(RoomEvent.ActiveSpeakersChanged, handleActiveSpeakerChange)
+            .on(RoomEvent.Disconnected, handleDisconnect)
+            .on(RoomEvent.LocalTrackUnpublished, handleLocalTrackUnpublished);
+            // this token should be pulled from the server
+            // as a streamer the user should be able to request a stream token
+            let token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2NzQyNDMxOTgsImlzcyI6ImRldmtleSIsIm5hbWUiOiJ1c2VyMSIsIm5iZiI6MTY3NDE1Njc5OCwic3ViIjoidXNlcjEiLCJ2aWRlbyI6eyJyb29tIjoibXktZmlyc3Qtcm9vbSIsInJvb21Kb2luIjp0cnVlfX0.pgNZOzNhdEjORfj0hmGAZhabM2O2tdcmLlgeF8edjO8';
+            // connect to room
+            // this should be configured via the ENV
+            await room.connect('wss://livekit.rnih.org', token);
+            console.log('connected to room', room.name);
+
+            // publish local camera and mic tracks
+            await room.localParticipant.enableCameraAndMicrophone();
+
+            
+            function handleTrackSubscribed(
+                track,
+                publication ,
+                participant,
+                ) {
+                if (track.kind === Track.Kind.Video || track.kind === Track.Kind.Audio) {
+                    // attach it to a new HTMLVideoElement or HTMLAudioElement
+                    const element = track.attach();
+                    parentElement.appendChild(element);
+                }
+            }
+
+            function handleTrackUnsubscribed(
+                track,
+                publication,
+                participant,
+                ) {
+                // remove tracks from all attached elements
+                track.detach();
+            }
+
+            function handleLocalTrackUnpublished(track, participant) {
+                // when local tracks are ended, update UI to remove them from rendering
+                track.detach();
+            }
+
+            function handleActiveSpeakerChange(speakers) {
+                // show UI indicators when participant is speaking
+            }
+
+            function handleDisconnect() {
+                console.log('disconnected from room');
+            }
+            */
+        },
+
+        stopStream(){
+            console.log("This should stop the stream if it's running.");
+            console.log(`WebRTC state: ${this.webRTC}`);
+        },
+        startStreamOldWebRTCMultiConnection(){
+
+
+
+
+            
             var broadcastId = 'MarkPugnerWasNeverHere';//document.getElementById('broadcast-id').value; // this should be our user id
             function parseToken (token) {
                 try{
@@ -482,15 +723,17 @@
                     if (!isBroadcastExists) {
                         // the first person (i.e. real-broadcaster) MUST set his user-id
                         connection.userid = broadcastId;
-                        let broadcastDesc = 'fatchat test description. Hard coded.'; //document.getElementById('broadcast-desc').value;
-                        // LOCALSTORAGE ADD FEATURE
-                        //if (localStorage.getItem('broadcast-desc')) {
-                        //    document.getElementById('broadcast-desc').value = localStorage.getItem('broadcast-desc');
-                        //}
+                        let broadcastDesc = 'fatchat test description. Hard coded.'; 
+
+                        // use a mix of local storage to save and load the values in the ui
+                        if(document.getElementById('stream-desc').value){
+                            broadcastDesc = document.getElementById('stream-desc').value;
+                        }
+                        
 
                         // set it as the next defaut stream title
-                        let rtmpstream = 'https://hls.robotstreamer.com/tv/4126.m3u8';
-                        //rtmpstream = document.getElementById('rtmp').value;
+                        let rtmpstream = 'https://hls/rtmpendpoint/site/stream.m3u8';
+                        rtmpstream = document.getElementById('stream-rtmp-alt').value;
                         localStorage.setItem('rtmp',rtmpstream);
                         localStorage.setItem('broadcast-desc',broadcastDesc);
                         socketlc.emit('announcestream',{desc:broadcastDesc,viewers:0,src:rtmpstream});
@@ -507,18 +750,70 @@
                         });
                     }
                 });
-            });
-            }
+            }); 
+            
+        
+        
+        }
+           
     },
 
     computed: {
       ...mapGetters( {
         username: VStore.$getters.getUsername,
+        //getUseWebRTC: VStore.$getters.getWebRTC,
       }),
+
+      // setUseWebRTC: Player.$mutations.setWebRTC,
+      /*
+      ...mapMutations(Player.namespace, {
+        //setPinToLive: Player.$mutations.setKeepLive,
+        setUseWebRTC: Player.$mutations.setWebRTC,
+        
+        //setDisableBumps: Player.$mutations.setDisableBumps,
+      }),*/
 
       ...mapState(ChatStore.namespace, {
         displayChat: ChatStore.$states.displayChat
       }),
+
+      /*
+      ...mapState(Player.namespace, {
+        
+        getUseWebRTC: Player.$states.webRTC,
+        
+      }),
+
+      ...mapActions( Player.namespace, {
+        loadPlayerSettingsStreamer: Player.$actions.loadSettings,
+      }),
+      */
+      
+      useWebRTC: {
+        
+        set ( val ) { this.setUseWebRTC( val ) },
+        get () {
+            
+            let webRTC = true;
+            //const webRTC = utils.readFromLocalStorage( 'webRTC' );
+            console.log(webRTC);
+            /*if ( webRTC ) {
+                commit( $mutations.setWebRTC, JSON.parse(webRTC) );
+                try {
+                    localStorage.removeItem( 'webRTC' );
+                } catch ( error ) {
+                    console.error( `Failed to remove 'webRTC' from localStorage`, error );
+                }
+                }*/
+             return this.webRTC;
+             }
+      },
+
+      loadRTC() {
+        // read it into this from the local storage because holy crap this is retarded
+        console.log("Should enable webrtc based on the local storage state");
+      },
+      
 
       mobile () {
         return this.mounted
@@ -528,7 +823,19 @@
     },
 
     mounted() {
+        let settings = localStorage.getItem('player-settings');
+            console.log("player settings Settings read:", settings);
+            let setting_obj = JSON.parse(settings)
+            this.webRTC = setting_obj.webRTC;
+            console.log("is webrtc enbaled: ", this.webRTC);
+
+            // LOCALSTORAGE ADD FEATURE
+            if (localStorage.getItem('broadcast-desc')) {
+                //document.getElementById('stream-desc').value = localStorage.getItem('broadcast-desc');
+            }
       this.mounted = true;
+      //this.loadPlayerSettingsStreamer();
+      //this.loadRTC();
     },
   };
 </script>
